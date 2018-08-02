47c83b3f7071aa53be1c82d41648ffa8b833d878
SrHuggest <41216249+SrHuggest@users.noreply.github.com>
2018-08-02 12:23:38 -0300

    Update index.js

--- index.js
+++ index.js
@@ -52,53 +52,6 @@ bot.on("guildCreate", async guild => {
 });
 
 
-bot.on('guildMemberAdd', async member => {
-    const Canvas = require('Canvas');
-    const snekfetch = require('snekfetch');
-    const applyText = (canvas, text) => {
-        const ctx = canvas.getContext('2d');
-        let fontSize = 70;
-
-        do {
-            ctx.font = `${fontSize -= 10}px sans-serif`;
-        } while (ctx.measureText(text).width > canvas.width - 300);
-
-        return ctx.font;
-    };
-    const channel = member.guild.channels.find(ch => ch.name === 'portao');
-    if (!channel) return;
-
-    const canvas = Canvas.createCanvas(700, 250);
-    const ctx = canvas.getContext('2d');
-
-    const background = await Canvas.loadImage('./wallpaper.jpg');
-    ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-
-    ctx.strokeStyle = '#74037b';
-    ctx.strokeRect(0, 0, canvas.width, canvas.height);
-
-    ctx.font = '28px Impact';
-    ctx.fillStyle = '#ffffff';
-    ctx.fillText('Seja bem vindo(a) ao servidor', canvas.width / 2.5, canvas.height / 3.5);
-
-    ctx.font = applyText(canvas, `${member.displayName}!`);
-    ctx.fillStyle = '#ffffff';
-    ctx.fillText(`${member.displayName}!`, canvas.width / 2.5, canvas.height / 1.8);
-
-    ctx.beginPath();
-    ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
-    ctx.closePath();
-    ctx.clip();
-
-    const { body: buffer } = await snekfetch.get(member.user.displayAvatarURL);
-    const avatar = await Canvas.loadImage(buffer);
-    ctx.drawImage(avatar, 25, 25, 200, 200);
-
-    const attachment = new Discord.Attachment(canvas.toBuffer(), 'welcome-image.png');
-
-    channel.send(`${member} `, attachment);
-});
-
 bot.on("message", message => {
     if(message.content.includes('<@467689941669904394>')){
   var embedz = new Discord.RichEmbed()
